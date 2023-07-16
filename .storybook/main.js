5367b49d036edc7629183b91b5607cbbdb3dc3af
Rajesh <broncha@rajesharma.com>
2023-07-16 19:06:31 +0545

    fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,5 +6,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  typescript: {
+    reactDocgen: 'react-docgen-typescript-plugin'
+  },
 }
