cb5089b69d42a6dc85e9439c782b0c154b23f0c7
Rajesh <broncha@rajesharma.com>
2023-11-06 16:45:36 +0545

    npm audit and dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,11 +3,22 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
+
   typescript: {
     reactDocgen: 'react-docgen-typescript-plugin'
   },
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  }
 }
